2442eca57b77db801e1b392a39f36dc3f7f9b912
Joshua Comeau <joshwcomeau@gmail.com>
2015-04-21 17:19:08 -0400

    Basic Percolator server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // DEFAULT API SERVER
+
+var Percolator = require('percolator').Percolator;
+var server = new Percolator();
+
+server.route('/cats', {
+  GET: function(req, res) {
+    res.object({ message: "Hello world!" }).send();
+  }
+});
+
+
+server.listen(function(err) {
+  console.log("Server is listening on port", server.port);
+});
