0e1c5edde2d0d2339c74f5ff044be0bc45205f93
Joshua Comeau <joshwcomeau@gmail.com>
2015-04-22 09:37:13 -0400

    Randomization implemented. Added a simple-random NPM package, and made it filter by URL params (just emotion for now)

--- app/models/Cat.js
+++ app/models/Cat.js
@@ -1,7 +1,11 @@
 var mongoose  = require('mongoose');
+var random    = require('mongoose-simple-random');
+
 var catSchema = mongoose.Schema({
   emotion: String,
   url:     String
 });
+
+catSchema.plugin(random);
 
 module.exports = mongoose.model('Cat', catSchema);
--- app/routes.js
+++ app/routes.js
@@ -2,23 +2,39 @@ var Cat = require('./models/Cat.js');
 
 // INDEX - GET /cats
 exports.index = function(req, res) {
-  var opts = req.uri.query();
+  var opts   = req.uri.query();
+  var filter = {};
 
-  console.log("Number of cats to show:", opts.num)
-  res.object({ message: "Hello world!" }).send();
+  if ( opts.emotion ) filter.emotion = opts.emotion;
+
+  var cat    = Cat.findOneRandom(filter, function(err, doc) {
+    if (err) {
+      return res.status.internalServerError(["Oh no, the server exploded:", err]);
+    } else {
+      // Handle the no-cats-found case
+      console.log(doc);
+      if (!doc) {
+        return res.status.notFound(["We don't have any cats that match your query:"]);
+      }
+      res.object(doc).send();
+    }
+  });
 }
 
 // SHOW - GET /cats/:id
 exports.show = function(req, res) {
+  // Get the ID from the URL parameter
   var catId = req.uri.child();
-  console.log("ID is ", catId);
 
+  // Look it up by its ID in Mongoose
   var cat = Cat.findById(catId, function(err, doc) {
+    // Respond with just the found document.
     res.object(doc).send();
   });
 
 }
 
+// CREATE - POST /cats
 exports.create = function(req, res) {
 
   var schema = {
@@ -35,17 +51,21 @@ exports.create = function(req, res) {
   };
 
   req.onJson(schema, function(err, obj) {
-    console.log("error:", err);
-    console.log("object:", obj);
+    // Error handling on schema failure is handled automatically by Percolator.
+    // Assuming the only possible error here is invalid JSON.
+    if (err) {
+      console.log("ERROR:", err);
+    } else {
 
-    var cat = new Cat(obj);
-    cat.save(function(mongoErr) {
-      if (mongoErr) {
-        return res.status.internalServerError(["We could not save the Cat:", mongoErr]);
-      } else {
-        res.object({ cat: cat }).send();
-      }
-    })
+      var cat = new Cat(obj);
+      cat.save(function(mongoErr) {
+        if (mongoErr) {
+          return res.status.internalServerError(["We could not save the Cat:", mongoErr]);
+        } else {
+          res.object({ cat: cat }).send();
+        }
+      });
+    }
   });
 
 
