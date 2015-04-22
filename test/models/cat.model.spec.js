73003cf4e8a44a5ff4cc71c0523635d974139d19
Joshua Comeau <joshwcomeau@gmail.com>
2015-04-22 12:44:02 -0400

    Added some basic model specs for Cat

---
+++ test/models/cat.model.spec.js
@@ -0,0 +1,55 @@
+// Vendor modules
+var mongoose = require("mongoose");
+var chai     = require("chai");
+
+// Local modules
+var Cat      = require("../../app/models/Cat.js");
+
+var expect   = chai.expect;
+var dbUrl    = "mongodb://localhost/RequestKittensTest";
+
+mongoose.connect(dbUrl);
+
+////// Tests Begin //////
+
+describe("Cat", function() {
+  var currentCat = null;
+
+  beforeEach(function(done) {
+    var cat = new Cat({
+      emotion: 'sad',
+      url:     'http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg'
+    });
+
+    cat.save(function(err, doc) {
+      if (err) return console.error(err);
+      currentCat = doc;
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    Cat.remove({}, function() {
+      done();
+    });
+  });
+
+  it("has persisted a cat entry", function(done) {
+    Cat.count({}, function(err, count) {
+      if (err) return console.error(err);
+      expect(count).to.equal(1);
+      done();
+    });
+  });
+
+  it("can be found by emotion", function(done) {
+    Cat.findOne({emotion: 'sad'}, function(err, cat) {
+      if (err) return console.error(err);
+
+      expect(cat.name).to.equal(currentCat.name);
+      done();
+    });
+  });
+
+
+});
